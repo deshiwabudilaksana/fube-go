20181d065b7b0b16bebb40eea78bd47d6caf6073
Deshiwa Budilaksana <deshiwa.budilaksana@igloohome.co>
2025-06-30 10:09:46 +0800

    add new endpoint create customer

--- handlers/customer_handlers.go
+++ handlers/customer_handlers.go
@@ -9,21 +9,35 @@ import (
 	"github.com/deshiwabudilaksana/fube-go/database"
 	"github.com/deshiwabudilaksana/fube-go/middlewares"
 	"github.com/deshiwabudilaksana/fube-go/models"
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
 
 func GetCustomerHandlers(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
-	customer, err := models.GetCustomer(vars["id"])
-	if err != nil {
-		if err.Error() == "customer not found" {
-			http.Error(w, "Customer not found", http.StatusNotFound)
-		} else {
-			http.Error(w, "Error retrieving customer data", http.StatusInternalServerError)
-		}
+	log.Println("vars >>>", vars["id"])
+
+	currentUser, ok := middlewares.GetUserFromContext(r.Context())
+
+	if !ok {
+		http.Error(w, "User data not found", http.StatusBadRequest)
 		return
 	}
+
+	var customer models.Customer
+	// database.DB.Where("vendor_id = ?", currentUser.Vendor.ID).Find(&allCustomer)
+
+	id, err := uuid.Parse(vars["id"])
+
+	if err != nil {
+		http.Error(w, "Invalid id", http.StatusBadRequest)
+		return
+	}
+
+	database.DB.Where(&models.Customer{ID: id, VendorID: currentUser.Vendor.ID}).First(&customer)
+
+	log.Println("customer found >>", customer)
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(customer)
@@ -46,26 +60,58 @@ func GetAllCustomersHandlers(w http.ResponseWriter, r *http.Request) {
 
 type CustomerInput struct {
 	Name      string
-	Birthdate string
+	Birthdate time.Time
 	Phone     string
 }
 
 func AddCustomer(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		http.Error(w, "Can not send empty request", http.StatusBadRequest)
-	}
+	currentUser, ok := middlewares.GetUserFromContext(r.Context())
 
-	customer := CustomerInput{
-		Name:      r.Form.Get("name"),
-		Birthdate: r.Form.Get("birthdate"),
-		Phone:     r.Form.Get("phone"),
+	if !ok {
+		http.Error(w, "User data not found", http.StatusBadRequest)
+		return
 	}
+	// This is how to get data from multipart form request
+	// if err := r.ParseForm(); err != nil {
+	// 	http.Error(w, "Can not send empty request", http.StatusBadRequest)
+	// }
+
+	// customer := CustomerInput{
+	// 	Name:      r.Form.Get("name"),
+	// 	Birthdate: r.Form.Get("birthdate"),
+	// 	Phone:     r.Form.Get("phone"),
+	// }
+
+	// log.Println("Customer data parsed >>>", customer)
 
 	//To do: add authentication
 
-	newCustomer := database.DB.Create(customer)
+	var input CustomerInput
 
-	if newCustomer.Error != nil {
+	// Decode JSON body into struct
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	log.Println("Customer data decoder >>>", input)
+
+	// newCustomer := database.DB.Create(customer)
+	newCustomer := models.Customer{
+		FullName:  input.Name,
+		Birthdate: input.Birthdate,
+		Phone:     input.Phone,
+		Points:    0,
+		VendorID:  currentUser.Vendor.ID,
+		IsRemoved: false,
+	}
+
+	result := database.DB.Create(&newCustomer)
+
+	log.Println("Result >>", result)
+
+	if result.Error != nil {
 		http.Error(w, "Error creating customer", http.StatusConflict)
 		return
 	}
@@ -80,9 +126,9 @@ func TestGetRequestBody(w http.ResponseWriter, r *http.Request) {
 	}
 
 	customer := CustomerInput{
-		Name:      r.Form.Get("name"),
-		Birthdate: r.Form.Get("birthdate"),
-		Phone:     r.Form.Get("phone"),
+		Name: r.Form.Get("name"),
+		// Birthdate: r.Form.Get("birthdate"),
+		Phone: r.Form.Get("phone"),
 	}
 
 	log.Println("get request >> ", customer)
